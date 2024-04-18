dcc2b4b890770fdf2625132bfa0c9fc4e517ed97
xiaoRuMiLi <2072836794@qq.com>
2024-04-18 21:38:41 +0800

    n

--- ai-workers/src/views/ai-worker/components/Menu.vue
+++ ai-workers/src/views/ai-worker/components/Menu.vue
@@ -2,8 +2,8 @@
  * @Description: 
  * @Author: lyq
  * @Date: 2024-04-18 17:32:37
- * @LastEditTime: 2024-04-18 18:49:59
- * @LastEditors: lyq
+ * @LastEditTime: 2024-04-18 21:31:50
+ * @LastEditors: Please set LastEditors
 -->
 <template>
     <div class="ai-worker-menu">
@@ -18,22 +18,13 @@
             <n-collapse arrow-placement="right">
                 <n-collapse-item title="青铜" name="1">
                     <template #header>
-                        <div class="chat-list-header"> 历史聊天记录 </div>
+                        <div class="chat-list-header"> 历史记录 </div>
                     </template>
                     <div class="chat-item">
                         <div class="name">
                             汽修厂服务员
                         </div>
-                        <div class="info">
-                            愉快的完成了关于购买汽车保险的对话，虽然有点波折，还好完成了
-                        </div>
-                        <div class="date">
-                            2024-01-30 14:52:00
-                        </div>
                     </div>
-                    <div class="chat-item">对话一</div>
-                    <div class="chat-item">对话一</div>
-                    <div class="chat-item">对话一</div>
                     <template #arrow>
                         <div class="list-item-arrow">
 
